0914c56e74e6d53f222cc64a4b28e86eb1998e40
ingridnorton <ingridnorton@hotmail.com>
2018-05-08 15:59:04 +1000

    created Asteroids Game

---
+++ Unity/Assets/~Asteroids/Scripts/Movement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Asteroids
+{
+    public class Movement : MonoBehaviour
+    {
+        public float speed = 20f; //Units to travel
+        public float rotationSpeed = 360f; //Amount of rotation per second
+
+
+        private Rigidbody2D rigid; // Refrence to attached RigidBody2D
+
+        // Use this for initialization
+        void Start()
+        {
+
+            //Tried to add force in the transform's up direction via speed
+            // rigid.AddForce(transform.up * speed); this code does not work
+
+            //Grab refrence to rigidbody2d component
+            //Note: It gets this from the GameObject thus script is attached to
+            rigid = GetComponent<Rigidbody2D>();
+
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //Check if the A key is pressed
+            if (Input.GetKey(KeyCode.A))
+            {
+                //Rotate Left
+                transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+            }
+            //Check if the D key is pressed
+            if (Input.GetKey(KeyCode.D))
+            {
+                //Rotate right
+                transform.Rotate(-Vector3.forward, rotationSpeed* Time.deltaTime);
+            }
+
+            //Check if the 'W' key is pressed
+            if (Input.GetKey(KeyCode.W))
+            {
+                rigid.AddForce(transform.up * speed);
+            }
+            //Check if the S key is pressed
+            if (Input.GetKey(KeyCode.S))
+            {
+                rigid.AddForce (-transform.up * speed);
+            }
+
+
+        }
+    }
+}
---
+++ Unity/Assets/~Asteroids/Scripts/Shooting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Aesteroids
+{
+    public class Shooting : MonoBehaviour
+    {
+        public GameObject bulletPrefab; //Prefab of bullet to spawn
+        public float bulletSpeed = 20f; //Speed that the bullet travels
+        public float shootRate = 0.2f; //Rate of fire (in seconds)
+
+        private float shootTimer = 0f; //Timer to count to shoot rate 
+    
+
+    void Shoot()
+    {
+        //Create a new bullet clone
+        GameObject clone = Instaniate(bulletPrefab, Transform position, Transform.rotation);
+        //Grab rigidbody from clone
+        Rigidbody2D rigid = clone.GetComponent<Rigidbody2D>();
+        //Add a force to the bullet (using bulletSpeed)
+        rigid.AddForce(Transform.up * bulletspeed, ForceMode2D.Impulse);
+    }
+    //Update is called once per frame
+    void Update()
+    {
+        //Set shootTimer = shootTimer
+    }
+        }
+}
+
+
