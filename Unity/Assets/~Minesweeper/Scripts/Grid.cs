3bbf37fc5d90b8f53a2e0c800af91e5378260ae0
ingridnorton <ingridnorton@hotmail.com>
2018-05-22 15:58:12 +1000

    started working on minesweeper

---
+++ Unity/Assets/~Minesweeper/Scripts/Grid.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper
+{
+    public class Grid : MonoBehaviour
+    {
+        //Functions & Variables go here
+        public GameObject tilePrefab;
+        public int width = 10, height = 10;
+        public float spacing = .155f;
+
+        private Tile[,] tiles;
+
+
+
+        Tile SpawnTile(Vector3 pos)
+        {
+            //Clone tile prefab
+            GameObject clone = Instantiate(tilePrefab);
+
+            //Edit it's properties
+            clone.transform.position = pos;
+            Tile currentTile = clone.GetComponent<Tile>();
+
+            //Return it
+            return currentTile;
+
+        }
+        void GenerateTiles()
+        {
+            //Create a new 2D array of size width x height
+            tiles = new Tile[width, height];
+            //Loop through the entire tile list
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    //NOTE: Part 2 goes here
+
+                    //Store half size for later use
+                    Vector2 halfSize = new Vector2(width * 0.5f,
+                                                   height * 0.5f);
+                    //Pivoy tiles around the grid
+                    Vector2 pos = new Vector2(x - halfSize.x,
+                                                y - halfSize.y);
+                    //Apply spacing
+                    pos *= spacing;
+
+                    //Spawn the tile using spawn functiuon made earlier
+                    Tile tile = SpawnTile(pos);
+
+                    //Sttach newly spawned tile to self (transform)
+                    tile.transform.SetParent(transform);
+
+                    //Store its array coordinates within itself for future refrence
+                    tile.x = x;
+                    tile.y = y;
+
+                    //Store tile in array at those coordinates
+                    tiles[x, y] = tile;
+
+                }
+            }
+        }
+    }
+}
---
+++ Unity/Assets/~Minesweeper/Scripts/Tile.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Tile : MonoBehaviour
+    {
+        //Functions and veriables go here
+        //Class needs to contain functions and variables
+        public int x, y;
+        public bool isMine = false; //Is the current tile a mine?
+        public bool isRevealed = false; //Has the tile already been revealed
+        [Header("Refrences")]
+        public Sprite[] emptySprites; // List of empty sprites i.e empty 1, 2, 3 ect
+        public Sprite[] mineSprites; //The mine sprites
+        private SpriteRenderer rend; //Refrence to the sprite render 
+
+        void Awake()
+        {
+            //Grab refrence to sprite renderer
+            rend = GetComponent<SpriteRenderer>();
+        }
+
+        void Start()
+        {
+            //randomly decide if this tile is a mine - using a 5% chance
+            isMine = Random.value < 0.5f;
+        }
+
+        public void Reveal(int adjacentMines, int mineState=0)
+        {
+            // Flags the tile as being revealed
+            isRevealed = true;
+            //Checks if tile is a mine
+            if (isMine)
+            {
+                // Sets sprite to mine sprite
+                rend.sprite = mineSprites[mineState];
+            }
+            else
+            {
+                //Sets sprite to approximate testure based in adjacent mines
+                rend.sprite = emptySprites[adjacentMines];
+            }
+        }
+    }
+}
