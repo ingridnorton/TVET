6eb064cdc3bae438fd12bf86c2cee5ecb829f965
ingridnorton <ingridnorton@hotmail.com>
2018-06-05 15:56:16 +1000

    kept going on asteroids

---
+++ Unity/Assets/Work/practice.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class practice : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        RaycastHit2D hit = Physics2D.Raycast();
+	}
+}
